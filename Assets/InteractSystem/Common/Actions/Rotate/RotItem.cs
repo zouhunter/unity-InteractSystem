427576125a63497c70389f36280a7d2f424d5b3c
zouhangte <zouhantezbm@126.com>
2018-06-13 20:18:18 +0800

    从备份中导入Rotate

---
+++ Assets/InteractSystem/Common/Actions/Rotate/RotItem.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions.Comparers;
+
+namespace InteractSystem.Common.Actions
+{
+    [AddComponentMenu(MenuName.RotObj)]
+    public class RotateItem : CompleteAbleActionItem
+    {
+        public float minAngle = -30;
+        public float maxAngle = 30;
+        public float triggerAngle = 30;
+        public bool clampHard;
+        public bool completeMoveBack;
+        public float autoCompleteTime = 2f;
+        [SerializeField]
+        private Transform _directionHolder;
+        [SerializeField]
+        private Transform _operater;
+        public Transform Operater { get { if (_operater == null) _operater = transform; return _operater; } }
+
+        public Vector3 Direction { get; private set; }
+
+        public override bool OperateAble
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private float currAngle;
+        private Quaternion startRot;
+        private FloatComparer comparer;
+        protected const float deviation = 1f;
+
+        protected override void Start()
+        {
+            base.Start();
+            InitLayer();
+            InitDirection();
+            comparer = new FloatComparer(deviation);
+        }
+
+        private void InitLayer()
+        {
+            GetComponentInChildren<Collider>().gameObject.layer = LayerMask.NameToLayer( Layers.rotateItemLayer);
+        }
+        private void InitDirection()
+        {
+            Direction = (_directionHolder.localPosition).normalized;//右手坐标系?
+            startRot = Operater.rotation;
+        }
+
+        public override void StepActive()
+        {
+            base.StepActive();
+            Operater.rotation = startRot;
+        }
+
+        private IEnumerator AutoRotateTo()
+        {
+            var target = Quaternion.Euler(Direction * triggerAngle) * startRot;
+            var start = Operater.rotation;
+            for (float timer = 0; timer < autoCompleteTime; timer += Time.deltaTime)
+            {
+                yield return null;
+                Operater.rotation = Quaternion.Lerp(start, target, timer/autoCompleteTime);
+            }
+            OnComplete();
+        }
+        public override void StepComplete()
+        {
+            base.StepComplete();
+            if (completeMoveBack)
+            {
+                Operater.rotation = startRot;
+            }
+        }
+        public override void StepUnDo()
+        {
+            base.StepUnDo();
+            Operater.rotation = startRot;
+            currAngle = 0;
+        }
+
+        internal bool TryMarchRot()
+        {
+            return comparer.Equals(currAngle, triggerAngle);
+        }
+
+        public void Clamp()
+        {
+            if (gameObject.activeInHierarchy)
+                StartCoroutine(Clamp(() => {
+                    if (TryMarchRot()){
+                        OnComplete();
+                    }
+                }));
+        }
+
+        private IEnumerator Clamp(UnityAction onComplete)
+        {
+            if (currAngle > maxAngle || currAngle < minAngle)
+            {
+                currAngle = Mathf.Clamp(currAngle, minAngle, maxAngle);
+                var target = Quaternion.Euler(Direction * currAngle) * startRot;
+                var start = Operater.rotation;
+                for (float timer = 0; timer < 1f; timer += Time.deltaTime)
+                {
+                    yield return null;
+                    Operater.rotation = Quaternion.Lerp(start, target, timer);
+                }
+            }
+            if (onComplete != null) onComplete.Invoke();
+        }
+
+        public void Rotate(float amount)
+        {
+            currAngle += amount;
+            if(clampHard)
+            {
+                if(currAngle < minAngle || currAngle > maxAngle)
+                {
+                    currAngle -= amount;
+                    return;
+                }
+            }
+            Operater.Rotate(Direction, amount, Space.World);
+        }
+
+        public override void AutoExecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Assets/InteractSystem/Common/Actions/Rotate/RotateNode.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+namespace InteractSystem.Common.Actions
+{
+    public class RotateNode : CompleteAbleCollectNode<RotateItem>,IRuntimeCtrl
+    {
+        public ControllerType CtrlType
+        {
+            get
+            {
+                return ControllerType.Rotate;
+            }
+        }
+    }
+}
